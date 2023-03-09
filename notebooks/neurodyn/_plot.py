cd567399f4cd0f5bcddbac2486329e41de173439
Nicole Vadot <ninivert1@gmail.com>
2023-03-09 13:31:50 +0100

    plotting method based on stats.binned_statistic2d

--- notebooks/neurodyn/_plot.py
+++ notebooks/neurodyn/_plot.py
@@ -18,21 +18,21 @@ def _unwrap_figax(figax: tuple[Figure, Axes] | None = None) -> tuple[Figure, Axe
 
 	return figax
 
-def plot_neuron_trajectory(res: OdeResult, n: int = 5, figax: tuple[Figure, Axes] | None = None) -> tuple[Figure, Axes]:
+def plot_neuron_trajectory(res: OdeResult, n: int = 5, figax: tuple[Figure, Axes] | None = None, **kwargs) -> tuple[Figure, Axes]:
 	fig, ax = _unwrap_figax(figax)
 
 	ax.set_xlabel('Time $t$ [s]')
 	ax.set_ylabel('Potential $h_i$ [V]')
 
 	for i in range(n):
-		ax.plot(res.t, res.y[i, :], label=f'${i=}$')
+		ax.plot(res.t, res.y[i, :], label=f'${i=}$', **kwargs)
 
 	ax.legend()
 
 	return fig, ax
 
 
-def plot_overlap_trajectory(rnn: LowRankRNN, res: OdeResult, figax: tuple[Figure, Axes] | None = None) -> tuple[Figure, Axes]:
+def plot_overlap_trajectory(rnn: LowRankRNN, res: OdeResult, figax: tuple[Figure, Axes] | None = None, **kwargs) -> tuple[Figure, Axes]:
 	fig, ax = _unwrap_figax(figax)
 
 	ax.set_xlabel('Time $t$ [s]')
@@ -41,22 +41,22 @@ def plot_overlap_trajectory(rnn: LowRankRNN, res: OdeResult, figax: tuple[Figure
 	m = overlap(rnn, res.y)
 
 	for mu in range(len(m)):
-		ax.plot(res.t, m[mu], label=f'$\\mu={mu}$')
+		ax.plot(res.t, m[mu], label=f'$\\mu={mu}$', **kwargs)
 
-	ax.legend()
 	ax.grid(axis='y')
+	ax.legend()
 
 	return fig, ax
 
 
-def plot_dh_hist(rnn: LowRankRNN, figax: tuple[Figure, Axes] | None = None) -> tuple[Figure, Axes]:
+def plot_dh_hist(rnn: LowRankRNN, figax: tuple[Figure, Axes] | None = None, **kwargs) -> tuple[Figure, Axes]:
 	fig, ax = _unwrap_figax(figax)
 
 	for mu in range(rnn.F.shape[1]):
-		ax.hist(rnn.dh(0, rnn.F[:, mu]), bins=30, histtype='step', label=f'$\\mu = {mu}$')
+		ax.hist(rnn.dh(0, rnn.F[:, mu]), bins=30, histtype='step', label=f'$\\mu = {mu}$', **kwargs)
 
 	ax.set_xlabel('$\\dot h_i$')
-	ax.legend()
 	ax.set_title('Derivative $\dot h_i$ at $\\xi_i^{\\mu}$')
+	ax.legend()
 
 	return fig, ax
