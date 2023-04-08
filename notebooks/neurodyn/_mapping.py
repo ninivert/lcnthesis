c18325e390a944ab59e87ca7c580024017f0847b
Nicole Vadot <ninivert1@gmail.com>
2023-04-08 12:03:16 +0200

    some wefactowwing !! :333 uwuuu

--- notebooks/neurodyn/_mapping.py
+++ notebooks/neurodyn/_mapping.py
@@ -6,79 +6,22 @@ from dataclasses import dataclass
 from abc import abstractmethod
 
 __all__ = [
+	'Box',
 	'Mapping', 'BinMapping',
-	'RecursiveQuadrantMapping', 'Box',
+	'RecursiveQuadrantMapping',
 	'ReshapeMapping',
 	'DiagonalMapping',
 	'FarMapping',
 	'LinearMapping',
 ]
 
-class Mapping:
-	"""Neural mapping from R² -> R"""
-
-	@abstractmethod
-	def __call__(self, F: np.ndarray) -> np.ndarray:
-		"""Apply the mapping of R² -> R
-		
-		Parameters
-		----------
-		F : np.ndarray of shape (N, 2)
-			2D embedding
-		
-		Returns
-		-------
-		np.ndarray of shape (N,)
-			1D embedding
-		"""
-		pass
-
-
-class BinMapping(Mapping):
-	"""Neural mapping from R² -> discrete bins in [0,1]"""
-
-	@property
-	@abstractmethod
-	def num_bins(self) -> int:
-		"""Total number of bins"""
-		pass
-
-	@abstractmethod
-	def inverse(self, alpha: np.ndarray) -> np.ndarray:
-		"""Inverse mapping from [0,1] -> R²
-		
-		Parameters
-		----------
-		alpha : np.ndarray of shape (N,)
-			1D embedding
-
-		Returns
-		-------
-		np.ndarray of shape (N, 2)
-			2D embedding
-		"""
-		pass
-
-	@abstractmethod
-	def indices(self, F: np.ndarray) -> np.ndarray:
-		"""Index of the bins corresponding to the mapping"""
-		pass
-
-	def binned_statistic(self, F: np.ndarray, h: np.ndarray, fill_na: float | None = 0.0) -> np.ndarray:
-		"""Compute the mean of `h` inside each bin"""
-		m = self.indices(F)
-		s = stats.binned_statistic(m, h, bins=range(self.num_bins+1)).statistic
-		if fill_na is not None:
-			s = np.nan_to_num(s, nan=fill_na)
-		return s
-
 
 @dataclass
 class Box:
-	xmin: float
-	xmax: float
-	ymin: float
-	ymax: float
+	xmin: float = 0.0
+	xmax: float = 1.0
+	ymin: float = 0.0
+	ymax: float = 1.0
 
 	@property
 	def xmid(self):
@@ -87,6 +30,14 @@ class Box:
 	@property
 	def ymid(self):
 		return (self.ymin+self.ymax)/2
+
+	@property
+	def xspan(self):
+		return self.xmax - self.xmin
+
+	@property
+	def yspan(self):
+		return self.ymax - self.ymin
 
 	def split_quadrants(self) -> tuple['Box', 'Box', 'Box', 'Box']:
 		return (
@@ -111,6 +62,10 @@ class Box:
 		"""
 		return (self.xmin <= points[:, 0]) & (points[:, 0] <= self.xmax) & (self.ymin <= points[:, 1]) & (points[:, 1] <= self.ymax)
 
+	def scale(self, points: np.ndarray) -> np.ndarray:
+		"""Given points in [0,1]², scale and translate back to points inside the bbox"""
+		return points * np.array([self.xspan, self.yspan]) + np.array([self.xmin, self.xmax])
+
 	@staticmethod
 	def new_bbox(points: np.ndarray) -> 'Box':
 		"""Construct a ``Box`` that bounds given ``points``
@@ -128,6 +83,122 @@ class Box:
 		return Box(points[:, 0].min(), points[:, 0].max(), points[:, 1].min(), points[:, 1].max())
 
 
+class Mapping:
+	"""Neural mapping from R² -> R"""
+
+	@abstractmethod
+	def __call__(self, F: np.ndarray, bbox: Box | None) -> np.ndarray:
+		"""Apply the mapping of R² -> R
+		
+		Parameters
+		----------
+		F : np.ndarray of shape (N, 2)
+			2D embedding
+		bbox : Box of None
+			bounding box to use. If None, will be set to the smallest bounding box of points ``F``
+		
+		Returns
+		-------
+		np.ndarray of shape (N,)
+			1D embedding
+		"""
+		pass
+
+
+class BinMapping(Mapping):
+	"""Neural mapping from R² -> discrete bins in [0,1]"""
+
+	def __init__(self, nx: int, ny: int):
+		"""
+		Parameters
+		----------
+		nx : int
+			number of bins in the `x` coordinate
+		ny : int
+			number of bins in the `y` coordinate
+		"""
+		self.nx = nx
+		self.ny = ny
+
+	def __call__(self, F: np.ndarray, bbox: Box | None = None) -> np.ndarray:
+		return self.indices(F, bbox) / self.num_bins
+
+	@abstractmethod
+	def indices(self, F: np.ndarray, bbox: Box | None = None) -> np.ndarray:
+		"""Index of the bins corresponding to the mapping"""
+		pass
+
+	def inverse(self, alpha: np.ndarray, bbox: Box = Box()) -> np.ndarray:
+		"""Inverse mapping from [0,1] -> R²
+
+		Note : this method suffers from floating points errors.
+		If indices are available, prefer using ``BinMapping.inverse_indices``.
+		
+		Parameters
+		----------
+		alpha : np.ndarray of shape (N,)
+			1D embedding
+		box : Box
+			bounding box for the points in R²
+
+		Returns
+		-------
+		np.ndarray of shape (N, 2)
+			2D embedding
+		"""
+		return self.inverse_indices((alpha*self.num_bins).astype(int))
+
+	def inverse_indices(self, indices: np.ndarray, bbox: Box = Box()) -> np.ndarray:
+		"""Compute point in 2D corresponding to the bins in 1D"""
+		return self.inverse_indices2d(self.indices_to_indices2d(indices), bbox)
+
+	@property
+	def num_bins(self) -> int:
+		"""Total number of bins"""
+		return self.nx * self.ny
+
+	def binned_statistic(self, F: np.ndarray, h: np.ndarray, fill_na: float | None = 0.0) -> np.ndarray:
+		"""Compute the mean of `h` inside each bin"""
+		m = self.indices(F)
+		s = stats.binned_statistic(m, h, bins=range(self.num_bins+1)).statistic
+		if fill_na is not None:
+			s = np.nan_to_num(s, nan=fill_na)
+		return s
+
+	def indices_to_indices2d(self, indices: np.ndarray) -> np.ndarray:
+		return np.vstack(np.unravel_index(indices, shape=(self.nx, self.ny))).T
+
+	def indices2d_to_indices(self, indices2d: np.ndarray) -> np.ndarray:
+		return np.ravel_multi_index(indices2d.T, dims=(self.nx, self.ny))
+
+	def inverse_indices2d(self, indices2d: np.ndarray, bbox: Box = Box()) -> np.ndarray:
+		return bbox.scale(indices2d / indices2d.max(axis=0))
+
+	def indices2d(self, F: np.ndarray, bbox: Box | None = None) -> np.ndarray:
+		"""Compute 2D indices of points ``F`` in R²
+		
+		Parameters
+		----------
+		F : np.ndarray of shape (N, 2)
+			points in R²
+		bbox : Box | None
+			bounding box of the points in R². If ``None``, set to be smallest bounding box of ``F``
+
+		Returns
+		-------
+		np.ndarray of ``int`` of shape (N, 2)
+		"""
+		if bbox is None:
+			bbox = Box.new_bbox(F)
+
+		return (np.vstack([
+			# NOTE : we need to clip, because we need equality on the right edge of the last bin : value <= bin[a-1]
+			np.clip(np.digitize(F[:, 0], bins=np.linspace(bbox.xmin, bbox.xmax, self.nx+1)), a_min=0, a_max=self.nx),
+			np.clip(np.digitize(F[:, 1], bins=np.linspace(bbox.ymin, bbox.ymax, self.ny+1)), a_min=0, a_max=self.ny),
+		]) - 1).T
+
+
+# FIXME !! am completely broken :((
 class RecursiveQuadrantMapping(BinMapping):
 	def __init__(self, n: int = 5):
 		"""Recursive quadrant mapping R² -> [0,1]
@@ -151,6 +222,7 @@ class RecursiveQuadrantMapping(BinMapping):
 
 	def inverse(self, coords: np.ndarray) -> np.ndarray:
 		"""Given coordinates (generated from ``coords`` or ``index_to_coords``), maps each point back to a bin in [0,1]²"""
+		# TODO : make me better
 		F = np.zeros((len(coords), 2))
 
 		for n in range(self.n):
@@ -168,7 +240,6 @@ class RecursiveQuadrantMapping(BinMapping):
 			Z[:, 0] = (Z[:, 0]-0.5)*(bbox.xmax - bbox.xmin)
 			Z[:, 1] = (Z[:, 1]-0.5)*(bbox.ymax - bbox.ymin)
 		return Z
-
 
 	def indices(self, F: np.ndarray) -> np.ndarray:
 		coords = self.coords(F, self.n)
@@ -289,32 +360,63 @@ class RecursiveQuadrantMapping(BinMapping):
 
 
 class ReshapeMapping(BinMapping):
-	def __init__(self, a: int, b: int) -> None:
-		super().__init__()
-
-		self.a = a
-		self.b = b
-
-	@property
-	def num_bins(self) -> int:
-		return self.a * self.b
-
-	def indices(self, F: np.ndarray) -> np.ndarray:
-		pass	
-
-	def inverse(self, alpha: np.ndarray) -> np.ndarray:
-		# TODO
-		pass
+	def indices(self, F: np.ndarray, bbox: Box | None = None) -> np.ndarray:
+		return self.indices2d_to_indices(self.indices2d(F, bbox))
 
 
 class DiagonalMapping(BinMapping):
-	# TODO
-	pass
+	def indices(self, F: np.ndarray, bbox: Box | None = None) -> np.ndarray:
+		indices2d = self.indices2d(F, bbox).T
+		indices_diag = (1/2*(indices2d[0] + indices2d[1])*(indices2d[0]+indices2d[1]+1)+indices2d[0]).astype(int)
+		# at this stage the diagonals are clipped, so we get something like
+		# 6  11 17 24
+		# 3  7  12 18
+		# 1  4  8  13
+		# 0  2  5  9
+		# -> we need to deduplicate and rename
+		_, idx_inverse = np.unique(indices_diag, return_inverse=True)
+		indices_diag = np.arange(self.nx*self.ny)[idx_inverse]
+		return indices_diag
 
 
 class FarMapping(BinMapping):
-	# TODO
-	pass
+	def __init__(self, nrec: int):
+		"""
+		Parameters
+		----------
+		nrec : int
+			number of recursions for the recursive quadrant split
+		"""
+		super().__init__(2**nrec, 2**nrec)
+		self.nrec = nrec
+
+	def indices(self, F: np.ndarray, bbox: Box | None = None) -> np.ndarray:
+		if bbox is None:
+			bbox = Box.new_bbox(F)
+		return self._far_indices(F, bbox, n=self.nrec)
+
+	def _far_indices(self, F: np.ndarray, bbox: Box, n: int, offset: int = 0, stride: int = 1) -> np.ndarray:
+		indices = np.zeros(len(F), dtype=int)
+
+		box1, box2, box4, box3 = bbox.split_quadrants()  # NOTE : reordering of the boxes
+
+		mask1 = (box1.xmin <= F[:, 0]) & (F[:, 0] <  box1.xmax) & (box1.ymin <= F[:, 1]) & (F[:, 1] <  box1.ymax)
+		mask2 = (box2.xmin <= F[:, 0]) & (F[:, 0] <= box2.xmax) & (box2.ymin <= F[:, 1]) & (F[:, 1] <  box2.ymax)
+		mask3 = (box3.xmin <= F[:, 0]) & (F[:, 0] <= box3.xmax) & (box3.ymin <= F[:, 1]) & (F[:, 1] <= box3.ymax)
+		mask4 = (box4.xmin <= F[:, 0]) & (F[:, 0] <  box4.xmax) & (box4.ymin <= F[:, 1]) & (F[:, 1] <= box4.ymax)
+
+		if n == 1:
+			indices[mask1] = offset+0*stride
+			indices[mask2] = offset+1*stride
+			indices[mask3] = offset+2*stride
+			indices[mask4] = offset+3*stride
+			return indices
+
+		indices[mask1] = self._far_indices(F[mask1, :], box1, n-1, offset=offset+0*stride, stride=stride*4)
+		indices[mask2] = self._far_indices(F[mask2, :], box2, n-1, offset=offset+1*stride, stride=stride*4)
+		indices[mask3] = self._far_indices(F[mask3, :], box3, n-1, offset=offset+2*stride, stride=stride*4)
+		indices[mask4] = self._far_indices(F[mask4, :], box4, n-1, offset=offset+3*stride, stride=stride*4)
+		return indices
 
 
 class LinearMapping(Mapping):
