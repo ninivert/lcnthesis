721aa29739dd068edd373cfe11781d640de25f5f
Nicole Vadot <ninivert1@gmail.com>
2023-04-11 10:38:57 +0200

    code cleanup

--- notebooks/neurodyn/_mapping.py
+++ notebooks/neurodyn/_mapping.py
@@ -155,6 +155,17 @@ class BinMapping(Mapping):
 	def inverse_indices2d(self, indices2d: np.ndarray, bbox: Box = Box()) -> np.ndarray:
 		return bbox.scale(indices2d / indices2d.max(axis=0))
 
+		# F = np.zeros((len(coords), 2))
+		# for n in range(self.n):
+		# 	offsets = np.array([[0.0, 0.0], [1/2**(n+1), 0.0], [1/2**(n+1), 1/2**(n+1)], [0.0, 1/2**(n+1)]])
+		# 	F += offsets[coords[:, n]-1]
+		# if centered:
+		# 	F += np.array([2**(-self.n)/2, 2**(-self.n)/2])  # center the 2D bins
+		# if bbox is not None:
+		# 	F[:, 0] = (F[:, 0]-0.5)*(bbox.xmax - bbox.xmin)
+		# 	F[:, 1] = (F[:, 1]-0.5)*(bbox.ymax - bbox.ymin)
+		# return F
+
 	@property
 	def num_bins(self) -> int:
 		"""Total number of bins"""
@@ -198,167 +209,6 @@ class BinMapping(Mapping):
 		]) - 1).T
 
 
-# TODO : move the j_coords methods to the new class
-# class RecursiveQuadrantMapping2(BinMapping):
-# 	def __init__(self, n: int = 5):
-# 		"""Recursive quadrant mapping R² -> [0,1]
-
-# 		See Pham 2018
-
-# 		Parameters
-# 		----------
-# 		n : int
-# 			number of iterations, by default 5.
-# 		"""
-# 		self.n = n
-
-# 	@property
-# 	def num_bins(self) -> int:
-# 		return 4**self.n
-	
-# 	def __call__(self, F: np.ndarray, box: Box | None = None) -> np.ndarray:
-# 		coords = self.coords(F, n=self.n, box=box)
-# 		return (coords-1) @ np.logspace(1, self.n, num=self.n, base=1/4)
-
-# 	def inverse(self, coords: np.ndarray) -> np.ndarray:
-# 		"""Given coordinates (generated from ``coords`` or ``index_to_coords``), maps each point back to a bin in [0,1]²"""
-# 		# TODO : make me better
-# 		F = np.zeros((len(coords), 2))
-
-# 		for n in range(self.n):
-# 			offsets = np.array([[0.0, 0.0], [1/2**(n+1), 0.0], [1/2**(n+1), 1/2**(n+1)], [0.0, 1/2**(n+1)]])
-# 			F += offsets[coords[:, n]-1]
-
-# 		return F
-
-# 	def inverse_samples(self, centered: bool = True, bbox: Box | None = None) -> np.ndarray:
-# 		"""Compute point in 2D corresponding to the bins in 1D"""
-# 		Z = self.inverse(self.indices_to_coords(np.arange(self.num_bins, dtype=int)))
-# 		if centered:
-# 			Z += np.array([2**(-self.n)/2, 2**(-self.n)/2])  # center the 2D bins
-# 		if bbox is not None:
-# 			Z[:, 0] = (Z[:, 0]-0.5)*(bbox.xmax - bbox.xmin)
-# 			Z[:, 1] = (Z[:, 1]-0.5)*(bbox.ymax - bbox.ymin)
-# 		return Z
-
-# 	def indices(self, F: np.ndarray) -> np.ndarray:
-# 		coords = self.coords(F, self.n)
-# 		return (coords-1) @ np.logspace(self.n-1, 0, num=self.n, base=4, dtype=int)
-
-# 	def indices_to_coords(self, indices: np.ndarray) -> np.ndarray:
-# 		"""Convert indices obtained from ``mapping_index`` to the corresponding quadrant coordinates"""
-# 		C = np.zeros((len(indices), self.n), dtype=int)
-# 		vals = indices.copy()
-
-# 		for n in reversed(range(0, self.n)):
-# 			C[:, n] = vals % 4
-# 			vals -= C[:, n]
-# 			vals >>= 0x2
-
-# 		C += 1
-# 		return C
-
-# 	def values_to_coords(self, values: np.ndarray) -> np.ndarray:
-# 		"""Convert values obtained from ``__call__`` to the corresponding quadrant coordinates.
-# 		When possible, prefer using ``mapping_index``, which avoid floating precision errors.
-		
-# 		Example
-# 		-------
-# 		Because of floating point errors, this assertion might fail
-
-# 		>>> rnn = LowRankRNN.new_valentin(p=2, N=20_000, phi=sigmoid, I_ext=zero, exclude_self_connections=True)
-# 		>>> mapping = RecursiveQuadrantMapping(n=6)
-# 		>>> (mapping.values_to_coords(indices) == mapping.coords()).all()
-# 		False
-
-# 		Using the indices however works
-
-# 		>>> (mapping.index_to_coords(indices) == mapping.coords()).all()
-# 		True
-# 		"""
-# 		# cast to index_to_coords algorithm
-# 		return self.indices_to_coords((values*self.num_bins).astype(int))
-
-# 		# base 4 algorithm
-# 		# C = np.zeros((len(values), self.n), dtype=int)
-# 		# vals = values.copy()
-# 		# 
-# 		# for n in range(0, self.n):
-# 		# 	vals *= 4
-# 		# 	C[:, n] = vals.astype(int)
-# 		# 	vals -= C[:, n]
-# 		# 
-# 		# C += 1
-# 		# return C
-
-# 	def coords(self, F: np.ndarray, n: int | None = None, box: Box | None = None) -> np.ndarray:
-# 		"""Generates coordinates (j1, ..., jn) for the recursive quadrant mapping box -> [0,1]
-		
-# 		Parameters
-# 		----------
-# 		F : np.ndarray of shape (N, p)
-# 			positions of the neurons in R^p
-# 		n : int or None
-# 			number of recursive iterations. If `None`, is set to ``self.n``
-# 		box : Box or None
-# 			bounding box for the mapping. If `None`, is set to the smallest bounding box that contains points ``F``
-
-# 		Returns
-# 		-------
-# 		np.ndarray of ``int`` of shape (N, n)
-# 			recursive quadrant coordinates for each neuron
-# 		"""
-
-# 		if n is None:
-# 			n = self.n
-		
-# 		coords = np.zeros((len(F), n), dtype=int)
-
-# 		# stopping condition
-# 		if len(F) == 0 or n == 0:
-# 			return coords
-
-# 		if box is None:
-# 			box = Box.new_bbox(F)
-# 		else:
-# 			assert box.xmin <= F[:, 0].min(), 'bbox does not contain all neurons'
-# 			assert F[:, 0].max() <= box.xmax, 'bbox does not contain all neurons'
-# 			assert box.ymin <= F[:, 1].min(), 'bbox does not contain all neurons'
-# 			assert F[:, 1].max() <= box.ymax, 'bbox does not contain all neurons'
-		
-# 		box1, box2, box3, box4 = box.split_quadrants()
-
-# 		# generate masks
-# 		# WARNING : we need to be careful with equality on the edges
-# 		# *---------*---------*
-# 		# |         |         |
-# 		# |  box4   |  box3   |
-# 		# |         |         |
-# 		# |---------*---------|
-# 		# |         |         |
-# 		# |  box1   |  box2   |
-# 		# |         |         |
-# 		# *---------*---------*
-# 		mask1 = (box1.xmin <= F[:, 0]) & (F[:, 0] <  box1.xmax) & (box1.ymin <= F[:, 1]) & (F[:, 1] <  box1.ymax)
-# 		mask2 = (box2.xmin <= F[:, 0]) & (F[:, 0] <= box2.xmax) & (box2.ymin <= F[:, 1]) & (F[:, 1] <  box2.ymax)
-# 		mask3 = (box3.xmin <= F[:, 0]) & (F[:, 0] <= box3.xmax) & (box3.ymin <= F[:, 1]) & (F[:, 1] <= box3.ymax)
-# 		mask4 = (box4.xmin <= F[:, 0]) & (F[:, 0] <  box4.xmax) & (box4.ymin <= F[:, 1]) & (F[:, 1] <= box4.ymax)
-
-# 		# generate coordinates for this layer
-# 		coords[mask1, 0] = 1
-# 		coords[mask2, 0] = 2
-# 		coords[mask3, 0] = 3
-# 		coords[mask4, 0] = 4
-
-# 		# call for coordinates of lower layers
-# 		coords[mask1, 1:] = self.coords(F[mask1, :], n-1, box1)
-# 		coords[mask2, 1:] = self.coords(F[mask2, :], n-1, box2)
-# 		coords[mask3, 1:] = self.coords(F[mask3, :], n-1, box3)
-# 		coords[mask4, 1:] = self.coords(F[mask4, :], n-1, box4)
-
-# 		return coords
-
-
 class RecursiveQuadrantMapping(BinMapping):
 	def __init__(self, nrec: int):
 		"""
@@ -373,10 +223,10 @@ class RecursiveQuadrantMapping(BinMapping):
 	def indices(self, F: np.ndarray, bbox: Box | None = None) -> np.ndarray:
 		if bbox is None:
 			bbox = Box.new_bbox(F)
-		coords = self._j_coords(F, bbox, self.nrec)
+		coords = self.j_coords(F, bbox, self.nrec)
 		return (coords-1) @ np.logspace(self.nrec-1, 0, num=self.nrec, base=4, dtype=int)
 
-	def _j_coords(self, F: np.ndarray, box: Box | None = None, n: int | None = None) -> np.ndarray:
+	def j_coords(self, F: np.ndarray, box: Box | None = None, n: int | None = None) -> np.ndarray:
 		"""Generates coordinates (j1, ..., jn) for the recursive quadrant mapping box -> [0,1]
 		
 		Parameters
@@ -436,12 +286,58 @@ class RecursiveQuadrantMapping(BinMapping):
 		coords[mask4, 0] = 4
 
 		# call for coordinates of lower layers
-		coords[mask1, 1:] = self._j_coords(F[mask1, :], box1, n-1)
-		coords[mask2, 1:] = self._j_coords(F[mask2, :], box2, n-1)
-		coords[mask3, 1:] = self._j_coords(F[mask3, :], box3, n-1)
-		coords[mask4, 1:] = self._j_coords(F[mask4, :], box4, n-1)
+		coords[mask1, 1:] = self.j_coords(F[mask1, :], box1, n-1)
+		coords[mask2, 1:] = self.j_coords(F[mask2, :], box2, n-1)
+		coords[mask3, 1:] = self.j_coords(F[mask3, :], box3, n-1)
+		coords[mask4, 1:] = self.j_coords(F[mask4, :], box4, n-1)
 
 		return coords
+
+	def indices_to_j_coords(self, indices: np.ndarray) -> np.ndarray:
+		"""Convert indices obtained from ``mapping_index`` to the corresponding quadrant coordinates"""
+		C = np.zeros((len(indices), self.n), dtype=int)
+		vals = indices.copy()
+
+		for n in reversed(range(0, self.n)):
+			C[:, n] = vals % 4
+			vals -= C[:, n]
+			vals >>= 0x2
+
+		C += 1
+		return C
+
+	def values_to_j_coords(self, values: np.ndarray) -> np.ndarray:
+		"""Convert values obtained from ``__call__`` to the corresponding quadrant coordinates.
+		When possible, prefer using ``mapping_index``, which avoid floating precision errors.
+		
+		Example
+		-------
+		Because of floating point errors, this assertion might fail
+
+		>>> rnn = LowRankRNN.new_valentin(p=2, N=20_000, phi=sigmoid, I_ext=zero, exclude_self_connections=True)
+		>>> mapping = RecursiveQuadrantMapping(n=6)
+		>>> (mapping.values_to_coords(indices) == mapping.coords()).all()
+		False
+
+		Using the indices however works
+
+		>>> (mapping.index_to_coords(indices) == mapping.coords()).all()
+		True
+		"""
+		# cast to index_to_coords algorithm
+		return self.indices_to_j_coords((values*self.num_bins).astype(int))
+
+		# base 4 algorithm
+		# C = np.zeros((len(values), self.n), dtype=int)
+		# vals = values.copy()
+		# 
+		# for n in range(0, self.n):
+		# 	vals *= 4
+		# 	C[:, n] = vals.astype(int)
+		# 	vals -= C[:, n]
+		# 
+		# C += 1
+		# return C
 
 
 class ReshapeMapping(BinMapping):
@@ -478,30 +374,31 @@ class FarMapping(BinMapping):
 	def indices(self, F: np.ndarray, bbox: Box | None = None) -> np.ndarray:
 		if bbox is None:
 			bbox = Box.new_bbox(F)
-		return self._far_indices(F, bbox, n=self.nrec)
 
-	def _far_indices(self, F: np.ndarray, bbox: Box, n: int, offset: int = 0, stride: int = 1) -> np.ndarray:
-		indices = np.zeros(len(F), dtype=int)
+		def _indices(F: np.ndarray, bbox: Box, n: int, offset: int = 0, stride: int = 1) -> np.ndarray:
+			indices = np.zeros(len(F), dtype=int)
 
-		box1, box2, box4, box3 = bbox.split_quadrants()  # NOTE : reordering of the boxes
+			box1, box2, box4, box3 = bbox.split_quadrants()  # NOTE : reordering of the boxes
 
-		mask1 = (box1.xmin <= F[:, 0]) & (F[:, 0] <  box1.xmax) & (box1.ymin <= F[:, 1]) & (F[:, 1] <  box1.ymax)
-		mask2 = (box2.xmin <= F[:, 0]) & (F[:, 0] <= box2.xmax) & (box2.ymin <= F[:, 1]) & (F[:, 1] <  box2.ymax)
-		mask3 = (box3.xmin <= F[:, 0]) & (F[:, 0] <= box3.xmax) & (box3.ymin <= F[:, 1]) & (F[:, 1] <= box3.ymax)
-		mask4 = (box4.xmin <= F[:, 0]) & (F[:, 0] <  box4.xmax) & (box4.ymin <= F[:, 1]) & (F[:, 1] <= box4.ymax)
+			mask1 = (box1.xmin <= F[:, 0]) & (F[:, 0] <  box1.xmax) & (box1.ymin <= F[:, 1]) & (F[:, 1] <  box1.ymax)
+			mask2 = (box2.xmin <= F[:, 0]) & (F[:, 0] <= box2.xmax) & (box2.ymin <= F[:, 1]) & (F[:, 1] <  box2.ymax)
+			mask3 = (box3.xmin <= F[:, 0]) & (F[:, 0] <= box3.xmax) & (box3.ymin <= F[:, 1]) & (F[:, 1] <= box3.ymax)
+			mask4 = (box4.xmin <= F[:, 0]) & (F[:, 0] <  box4.xmax) & (box4.ymin <= F[:, 1]) & (F[:, 1] <= box4.ymax)
 
-		if n == 1:
-			indices[mask1] = offset+0*stride
-			indices[mask2] = offset+1*stride
-			indices[mask3] = offset+2*stride
-			indices[mask4] = offset+3*stride
+			if n == 1:
+				indices[mask1] = offset+0*stride
+				indices[mask2] = offset+1*stride
+				indices[mask3] = offset+2*stride
+				indices[mask4] = offset+3*stride
+				return indices
+
+			indices[mask1] = _indices(F[mask1, :], box1, n-1, offset=offset+0*stride, stride=stride*4)
+			indices[mask2] = _indices(F[mask2, :], box2, n-1, offset=offset+1*stride, stride=stride*4)
+			indices[mask3] = _indices(F[mask3, :], box3, n-1, offset=offset+2*stride, stride=stride*4)
+			indices[mask4] = _indices(F[mask4, :], box4, n-1, offset=offset+3*stride, stride=stride*4)
 			return indices
 
-		indices[mask1] = self._far_indices(F[mask1, :], box1, n-1, offset=offset+0*stride, stride=stride*4)
-		indices[mask2] = self._far_indices(F[mask2, :], box2, n-1, offset=offset+1*stride, stride=stride*4)
-		indices[mask3] = self._far_indices(F[mask3, :], box3, n-1, offset=offset+2*stride, stride=stride*4)
-		indices[mask4] = self._far_indices(F[mask4, :], box4, n-1, offset=offset+3*stride, stride=stride*4)
-		return indices
+		return _indices(F, bbox, n=self.nrec)
 
 
 class LinearMapping(Mapping):
