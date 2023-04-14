2fc6b62103053d638cf28b15c6ab8c32bd529aaf
Nicole Vadot <ninivert1@gmail.com>
2023-04-14 18:01:45 +0200

    d=1 embedding notebook cleanup

--- notebooks/neurodyn/_anim.py
+++ notebooks/neurodyn/_anim.py
@@ -7,10 +7,63 @@ from math import ceil
 from ._rnn import LowRankRNN, Result
 from ._plot import plot_overlap_trajectory, scale_lightness
 
-__all__ = ['animate2d']
+__all__ = ['animate1d', 'animate2d']
 
 def midpoints(arr):
 	return (arr[:-1] + arr[1:]) / 2
+
+def animate1d(rnn: LowRankRNN, res: Result, outpath: Path, time_stride: int = 1, Nmax = 1500):
+	idt = 0
+	activity = rnn.phi(res.h[:Nmax])
+	cmap = mpl.colormaps['RdBu_r']
+	idx_mapping = np.arange(len(res.h[:Nmax, -1]))
+
+	fig, axes = plt.subplot_mosaic([
+		['a'],
+		['b'],
+		['m'],
+	], figsize=(6, 8), constrained_layout=True)
+
+	# neuron index
+
+	axes['a'].set_title('1D embedding based on neuron index')
+	sc_idx = axes['a'].scatter(idx_mapping, activity[:, idt], c='k', alpha=0.4, s=3, ec=None)
+	axes['a'].set_xlabel('neuron index $i$')
+	axes['a'].set_ylabel('Activity $A = \\phi(h_i)$ [Hz]')
+	axes['a'].set_ylim((0,1))
+
+	# neuron embedding
+
+	axes['b'].set_title('1D embedding based on neuron pattern $\\xi_i^0$')
+	sc_emb = axes['b'].scatter(rnn.F[:Nmax, 0], activity[:, idt], c='k', alpha=0.2, s=3, ec=None)
+	axes['b'].set_xlabel('neuron embedding $\\xi_i^0$')
+	axes['b'].set_ylabel('Activity $A = \\phi(h_i)$ [Hz]')
+	axes['b'].set_ylim((0,1))
+
+	# trajectory
+
+	plot_overlap_trajectory(rnn, res, figax=(fig, axes['m']))
+	line = axes['m'].axvline(res.t[idt], color='tab:gray', linestyle='--')
+
+	# we need a container to hold references
+	things = { 'line': line, 'sc_idx': sc_idx, 'sc_emb': sc_emb }
+
+	with tqdm(total=ceil(len(res.t)/time_stride)+1) as pbar:
+		def update(idt: int):
+			# update the line
+			things['line'].set_xdata([res.t[idt], res.t[idt]])
+
+			# update the scatters
+			sc_idx.set_offsets(np.vstack((idx_mapping, activity[:, idt])).T)
+			sc_emb.set_offsets(np.vstack((rnn.F[:Nmax, 0], activity[:, idt])).T)
+
+			pbar.update(1)
+
+		ani = animation.FuncAnimation(fig, update, frames=range(0, len(res.t), time_stride))
+		ani.save(outpath, writer='ffmpeg', fps=15)
+
+		plt.close()
+
 
 def animate2d(rnn: LowRankRNN, res: Result, outpath: Path, time_stride: int = 1, Nmax = 1500):
 	idt = 0
